bba8f31e263f363161d6c826eab7aaf2a7056189
glissa <Guillin>
2019-04-16 00:00:07 +0000

    Creacion del repositorio jigsaw - kaggle competition

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detect toxicity across a diverse range of conversations',
+    author='Guillin',
+    license='MIT',
+)
